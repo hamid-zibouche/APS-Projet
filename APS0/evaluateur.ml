f3f30abc428651d8363094de152991f674cb7c7e
fa214153 <awwal-ronald-oriade.fagbehouro@etu.univ-lehavre.fr>
2025-02-19 11:50:10 +0100

    évaluation des commandes et du programme

--- APS0/evaluateur.ml
+++ APS0/evaluateur.ml
@@ -26,10 +26,15 @@ let rec getNlist i list =
   [] -> failwith "la liste n'est pas assez grande"
   |(t::q) -> if(i = 1) then t else getNlist (i - 1) q
 
-  let valIntToInt x =
-    match x with 
-    |InZ(e) -> e 
-    | _ -> failwith "valIntToInt : Valeur inattendue (pas un entier)"
+let valIntToInt x =
+  match x with 
+  |InZ(e) -> e 
+  | _ -> failwith "valIntToInt : Valeur inattendue (pas un entier)"
+
+let string_of_val = function
+| InZ n -> string_of_int n
+| InF (_, args, _) -> "<function> with args: " ^ String.concat ", " args
+| InFR (_, name, args, _) -> "<recursive function " ^ name ^ "> with args: " ^ String.concat ", " args
 
 (* evaluation de chaque expression *)
 let rec evalExpr exp env =
@@ -52,24 +57,25 @@ let rec evalExpr exp env =
       |ASTId ("div") -> InZ(valIntToInt (evalExpr (getNlist 1 exprs) env) / valIntToInt(evalExpr (getNlist 2 exprs ) env))
       |_ ->
         (match (evalExpr func env) with
-          InF(eprime, argsfun, envF) ->  evalExpr eprime (ajouteArgsEnv argsfun exprs envF)
+          InF(eprime, argsfun, envF) ->  evalExpr eprime (ajouteArgsEnv argsfun exprs envF env)
           |InFR(eprime,id ,argsfun, envF) -> evalExpr eprime ( (id,InFR(eprime,id ,argsfun, envF))
-                                            :: ajouteArgsEnv argsfun exprs envF)
-        )
+                                            :: ajouteArgsEnv argsfun exprs envF env)
+        ) 
     )
   |ASTId (e) -> 
     (try 
     let valeur = List.assoc e env in valeur
   with Not_found -> failwith("Variable non definie:" ^ e)) and
 
-  ajouteArgsEnv args exprs env =
+  ajouteArgsEnv args exprs env envCours=
     match (args, exprs) with
     | ([],[]) -> env
     | ([],_) -> failwith "Trop d'arguments"
     | (_,[]) -> failwith "Pas assez d'arguments"
-    | (a1::aq,e1::eq) -> let new_env = ajouteArgsEnv aq eq env in
-                        (a1, evalExpr e1 env) :: List.remove_assoc a1 new_env
+    | (a1::aq,e1::eq) -> let new_env = ajouteArgsEnv aq eq env envCours in
+                        (a1, evalExpr e1 envCours) :: List.remove_assoc a1 new_env
 
+(* evaluation de chaque commande *)
 let rec evalInst inst env sortie = 
   match inst with
   ASTEcho(n) -> sortie @ [evalExpr n env]
@@ -81,68 +87,27 @@ let rec evalCmd cmd env sortie =
   |ASTFunRec(ASTId(s),_,args,e) -> ((s, InFR( e ,s, sorteArgs args ,env)) :: List.remove_assoc s env , sortie)
   |ASTStat(e) -> (env, evalInst e env sortie)
 
+(* evaluation de la suite de commandes *)
+let rec evalCmds cmds env sortie =
+  match cmds with
+  | [] -> sortie
+  | cmd::q ->( match (evalCmd cmd env sortie) with
+      | (e, s) -> s @ (evalCmds q e sortie))
 
-(* Environnement initial *)
-let env = [
-  ("true", InZ 1);
-  ("false", InZ 0);
-]
+(* evaluation du programme *)
+let rec evalProg prog = evalCmds prog [] [] ;;
 
-(* Fonction pour afficher une valeur *)
-let string_of_val = function
-  | InZ n -> string_of_int n
-  | InF (_, args, _) -> "<function> with args: " ^ String.concat ", " args
-  | InFR (_, name, args, _) -> "<recursive function " ^ name ^ "> with args: " ^ String.concat ", " args
+let rec afficheliste l = 
+  match l with 
+  |[]->""
+  |(InZ n) :: q -> (string_of_int n) ^ "\n" ^ (afficheliste q);;
 
-(* Fonction de test *)
-let run_test name expr env =
-  print_endline ("Test: " ^ name);
-  let result = evalExpr expr env in
-  print_endline ("Result: " ^ string_of_val result);
-  print_endline "------------------------"
-
-(* Tests *)
-let () =
-  run_test "Évaluation d’un entier" (ASTNum 42) env;
-  run_test "Opération logique AND" (ASTAnd (ASTId "true", ASTId "false")) env;
-  run_test "Opération logique OR" (ASTOr (ASTId "false", ASTId "true")) env;
-  run_test "Condition if-then-else (true)" (ASTIf (ASTId "true", ASTNum 100, ASTNum 500)) env;
-  run_test "Condition if-then-else (false)" (ASTIf (ASTId "false", ASTNum 100, ASTNum 500)) env;
-  run_test "Addition" (ASTApp (ASTId "add", [ASTNum 10; ASTNum 5])) env;
-
-(* Test avec variable *)
-  let env_with_x = ("x", InZ 5) :: env in
-  run_test "Récupération de variable" (ASTId "x") env_with_x; 
-
-   (* Fonction lambda *)
-  let lambda = ASTFerm ([ASTArg ("x", ASTInt)], ASTId "x") in
-  run_test "Fonction lambda" lambda env; 
-
-   (* Application de fonction *)
-  let applied_lambda = ASTApp (lambda, [ASTNum 10]) in
-  run_test "Application de fonction" applied_lambda env; 
-  
-
-  (* test eq *)
-   let applied_lambda2 = (ASTApp (ASTId "eq", [ASTNum 10; ASTNum 5])) in
-  run_test "Egalite" applied_lambda2 env; 
-
-  (* test lt *)
-  let applied_lambda2 = (ASTApp (ASTId "lt", [ASTNum 4; ASTNum 5])) in
-  run_test "less then" applied_lambda2 env; 
-
-  (* test not *)
-  let applied_lambda2 = (ASTApp (ASTId "not", [ASTId "false"])) in
-  run_test "not" applied_lambda2 env; 
-
-  (* test sub *)
-  let applied_lambda2 = (ASTApp (ASTId "sub", [ASTNum 10 ; ASTNum 13])) in
-  run_test "sub" applied_lambda2 env; 
-
-  (* test mul *)
-  let applied_lambda2 = (ASTApp (ASTId "mul", [ASTNum 10 ; ASTNum 13])) in
-  run_test "mul" applied_lambda2 env; 
-
-  (* test mul *)
-  let applied_lambda2 = (ASTApp (ASTId "div", [ASTNum 130 ; ASTNum 10])) in
-  run_test "div" applied_lambda2 env; 
+let fname = Sys.argv.(1) in
+let ic = open_in fname in
+  try
+    let lexbuf = Lexing.from_channel ic in
+    let p = Parser.prog Lexer.token lexbuf in
+      print_string ( afficheliste (evalProg p) );
+      print_string ".\n"
+  with Lexer.Eof ->
+    exit 0
